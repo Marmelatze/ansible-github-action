c91890316cca1999ef95bf5501886e4e59bce934
Florian Pfitzer <florian@pfitzer.me>
2020-10-31 17:20:08 +0100

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 mkdir -p ~/.ssk || true
 echo $KNOWN_HOSTS >> ~/.ssk/known_hosts
 
+echo "foo"
 eval `ssh-agent -s`
+echo "$SSH_PRIVATE_KEY"
 ssh-add - <<< "$SSH_PRIVATE_KEY"
 
 ansible-galaxy install -r ansible/requirements.yml
